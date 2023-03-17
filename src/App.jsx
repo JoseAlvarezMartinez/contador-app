ebaa2cf4e092f24d609e76146d7bd76274833cb2
JoseAlvarezMartinez <alvarezmjose2021@gmail.com>
2023-03-17 12:52:12 -0300

    -- Estilos Header --

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
-
+import styles from "./App.module.css";
+import Montania from "./assets/pattern-hills.svg";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <header className={styles.header}>
+      <div className={styles.montaniaContainer}>
+        <img
+          className={styles.montania}
+          src={Montania}
+          alt="Imagen de la Montania"
+        />
+      </div>
+    </header>
   );
 }
 
