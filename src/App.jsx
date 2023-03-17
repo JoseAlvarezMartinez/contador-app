d06f8c8807c3c56d6d0d01414b891e182962098d
JoseAlvarezMartinez <alvarezmjose2021@gmail.com>
2023-03-17 13:33:27 -0300

    -- setInterval --

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,28 @@
 import styles from "./App.module.css";
 import Montania from "./assets/pattern-hills.svg";
+import { useEffect, useState } from "react";
 function App() {
+  const [date, setDate] = useState(new Date());
+  useEffect(() => {
+    setInterval(() => {
+      setDate(new Date());
+    }, 1000)
+  });
+
   return (
     <header className={styles.header}>
+      <div className={styles.container}>
+        <div className={styles.cardTime}>
+          <p>Hours</p>
+        </div>
+        {date.toLocaleTimeString()}
+        <div className={styles.cardTime}>
+          <p>Minutes</p>
+        </div>
+        <div className={styles.cardTime}>
+          <p>Seconds</p>
+        </div>
+      </div>
       <div className={styles.montaniaContainer}>
         <img
           className={styles.montania}
