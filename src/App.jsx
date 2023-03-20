e4370c55ea60f01d3a4821cbc8de836fa6a0c8ad
JoseAlvarezMartinez <alvarezmjose2021@gmail.com>
2023-03-20 10:41:27 -0300

    -- Modal --

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,27 @@ import Montania from "./assets/pattern-hills.svg";
 import { useEffect, useState } from "react";
 function App() {
   const [date, setDate] = useState(new Date());
-
+  const [cronometro, setCronometro] = useState(0);
   useEffect(() => {
     setInterval(() => {
       setDate(new Date());
     }, 1000);
   });
-
+  useEffect(() => {
+    if (cronometro > 1) {
+      alert("funca");
+    }
+  }, [cronometro]);
   return (
     <header className={styles.header}>
       <div className={styles.container}>
         <div className={styles.hourCard}>
-          <h1 className={styles.h1}>{date.toLocaleTimeString()}</h1>
+          {cronometro.length ? (
+            <h1 className={styles.h1}>{date.toLocaleTimeString()}</h1>
+          ) : (
+            <h1 className={styles.h1}>{"00:00:00"}</h1>
+            // <h1 className={styles.h1}>{date.toLocaleTimeString()}</h1>
+          )}
         </div>
       </div>
       <div className={styles.montaniaContainer}>
@@ -23,6 +32,9 @@ function App() {
           src={Montania}
           alt="Imagen de la Montania"
         />
+      </div>
+      <div className={styles.addBtn}>
+        <p onClick={() => setCronometro(10)}>+</p>
       </div>
     </header>
   );
